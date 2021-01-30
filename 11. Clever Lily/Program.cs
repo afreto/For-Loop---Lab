cf93088592985acb7d1bd2632bc159d04980aa44
Момчил Миланов <deadman_kula@abv.bg>
2021-01-30 18:47:24 +0200

    add input

--- 11. Clever Lily/Program.cs
+++ 11. Clever Lily/Program.cs
@@ -9,6 +9,7 @@ namespace _11._Clever_Lily
             int age = int.Parse(Console.ReadLine());
             double washMashinePrice = double.Parse(Console.ReadLine());
             double toyPrice = double.Parse(Console.ReadLine());
+
         }
     }
 }
