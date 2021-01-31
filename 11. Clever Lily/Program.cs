393e2d2790ee29bf4b87eb96338df4d310f84ca0
Момчил Миланов <deadman_kula@abv.bg>
2021-01-31 18:31:03 +0200

    q

--- 11. Clever Lily/Program.cs
+++ 11. Clever Lily/Program.cs
@@ -9,6 +9,39 @@ namespace _11._Clever_Lily
             int age = int.Parse(Console.ReadLine());
             double washMashinePrice = double.Parse(Console.ReadLine());
             double toyPrice = double.Parse(Console.ReadLine());
+            double savedMoney= 0;
+            int moneyToGive= 10;
+            int numToys = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+
+                if (i%2==0)
+                {
+                    savedMoney += moneyToGive-1;
+                    moneyToGive += 10;
+
+                }
+                else
+                {
+                    numToys++;
+                }
+
+            }
+
+            savedMoney += numToys * toyPrice;
+
+            if (savedMoney>=washMashinePrice)
+            {
+                double diff = savedMoney - washMashinePrice;
+                Console.WriteLine($"Yes! {diff:f2}");
+            }
+            else
+            {
+                double diff = washMashinePrice - savedMoney;
+                Console.WriteLine($"No! {diff:f2}");
+            }
+
 
 
         }
