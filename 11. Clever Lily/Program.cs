2416dc5574283f5e36edd5b7413519cbb17cd7d1
Момчил Миланов <deadman_kula@abv.bg>
2021-02-02 17:22:02 +0200

    solved

--- 11. Clever Lily/Program.cs
+++ 11. Clever Lily/Program.cs
@@ -5,7 +5,8 @@ namespace _11._Clever_Lily
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+
             int age = int.Parse(Console.ReadLine());
             double washMashinePrice = double.Parse(Console.ReadLine());
             double toyPrice = double.Parse(Console.ReadLine());
